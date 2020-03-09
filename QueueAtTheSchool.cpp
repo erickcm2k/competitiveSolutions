3211d5aadbe3254aa019cfad8175250072f7d4cb
Erick Castañeda Martínez <erickce40@gmail.com>
2020-03-08 23:35:12 -0600

    queueAtTheSchool accepted

--- QueueAtTheSchool.cpp
+++ QueueAtTheSchool.cpp
@@ -10,5 +10,19 @@ int main(int argc, char const *argv[]) {
     cin >> n >> t;
     cin >> s;
 
+    for(int i = 0; i < t; i++) {
+        for(int j = 0; j < s.length() - 1; j++) {
+
+            if(s.at(j) == 'B' and s.at(j + 1) == 'G') {
+                s.at(j) = 'G';
+                s.at(j + 1) = 'B';
+                j++;
+            }
+
+        }
+        //cout << "t = " << i + 1 << " -> "<< s << endl;
+    }
+    cout << s << endl;
+    
     return 0;
 }
